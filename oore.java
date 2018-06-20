8a4b97c146f93f2fe54305c5c042c97a16292ee8
arjunanTJ <arjunanduke@gmail.com>
2018-06-20 11:06:47 +0530

    Update oore.java

--- oore.java
+++ oore.java
@@ -3,7 +3,9 @@ class Oore{
     int N;
     if(N>0)
       System.out.println("positive");
-    else
+    else if(N<0)
       System.out.println("negative");
+    else
+      System.out.println("zero");
   }
 }
