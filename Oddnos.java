a5eae8f443503f6e1aff44a04d2030785632d759
arjunanTJ <arjunanduke@gmail.com>
2018-07-07 08:09:14 +0530

    Update Oddnos.java

--- Oddnos.java
+++ Oddnos.java
@@ -4,7 +4,7 @@ class Oddnos{
 		Scanner s= new Scanner(System.in);
 		int n1=s.nextInt();
 		int n2=s.nextInt();
-		if(n1<n2){
+		if(true){
 			for(int i= n1+1;i<n2;i++){
 				if(i%2!=0){
 					System.out.print(" "+i);
